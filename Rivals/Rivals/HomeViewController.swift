e1db605d4a10fde8a12882b8f9b85bdef690e05b
X Code User <xcodeuser@admins-iMac-340.local>
2017-04-05 11:40:28 -0400

    Beginning to setup home screen

--- Rivals/Rivals/HomeViewController.swift
+++ Rivals/Rivals/HomeViewController.swift
@@ -11,22 +11,24 @@ import Firebase
 
 class HomeViewController: UIViewController {
 
+    var ref: FIRDatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        ref = FIRDatabase.database().reference()
         
         
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+
     }
  
 
